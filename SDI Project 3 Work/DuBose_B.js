a4f6d6c5223dad8d4dca261a37212a5c86ade02a
Brannon DuBosee <brannondubose@me.com>
2013-10-19 21:08:43 -0500

    added forumla for cost per day

--- SDI Project 3 Work/DuBose_B.js
+++ SDI Project 3 Work/DuBose_B.js
@@ -4,7 +4,9 @@
 var milesPerDay = 30;
 var gasBudget = 30;
 var withinBudget = true;
-var gasPrice = 3.50;
+var gasPrice = 3.50; 
+var CBRPrice = 5999; 
+
 
 // JSON Data, data for each vehicle
 var json = {
@@ -67,5 +69,25 @@ for (var i=0, j=dayOfWeek.length; i < j; i++) {
 	console.log( "The forcast for " + dayOfWeek[i] + " is " + weatherForcast[i] );	
 };
 
+var monthlyPayment = function (milesPerDay) {
+	
+	var myPayment = function (numerator, denominator) { 
+		var fraction = numerator / denominator;
+		return (fraction === Math.floor(fraction));			
+	};
 
 	
+	console.log("To drive the " + json.whatvehicle[0].vehicle + " to work, it costs " + (
+					(milesPerDay / json.whatvehicle[0].MPG) * gasPrice) + " dollars per day!");	
+	console.log("To drive the " + json.whatvehicle[1].vehicle + " to work, it costs " + (
+					(milesPerDay / json.whatvehicle[1].MPG) * gasPrice) + " dollars per day!");	
+	console.log("To drive the " + json.whatvehicle[2].vehicle + " to work, it costs " + (
+					(milesPerDay / json.whatvehicle[2].MPG) * gasPrice) + " dollars per day!");	
+
+};
+
+
+payment = monthlyPayment(milesPerDay)
+
+
+
