b9ed3488c956d6212ee458aa2765db78044df44e
Brannon DuBosee <brannondubose@me.com>
2013-10-19 20:21:20 -0500

    Added some more variables and console logs

--- SDI Project 3 Work/DuBose_B.js
+++ SDI Project 3 Work/DuBose_B.js
@@ -1,21 +1,27 @@
 //alert("JavaScript works!");
 
-// JSON Data, weather declarations	
+
+var milesPerDay = 30;
+var gasBudget = 30;
+var withinBudget = true;
+var gasPrice = 3.50;
+
+// JSON Data, data for each vehicle
 var json = {
 	"whatvehicle": [
 		{
-				"weather": "Sunny", 
 				"vehicle": "CBR500r",
+				"weather": "Sunny", 
 				"MPG": 70			
 		},
 		{
-				"weather": "Cloudy", 
 				"vehicle": "Mustang",
+				"weather": "Cloudy", 			
 				"MPG": 28
 		},
 		{
-				"weather": "Storming", 
 				"vehicle": "Jeep",
+				"weather": "Storming", 			
 				"MPG": 11							
 		}
 	]	
@@ -24,24 +30,42 @@ var json = {
 var jsonData = function (json) {
 	for (var i = 0; i < json.whatvehicle.length; i++){
 		var whatvehicle = json.whatvehicle[i];
-		console.log("Weather: " + whatvehicle.weather + ", Vehicle: " + whatvehicle.vehicle + ", MPG: " + whatvehicle.MPG);
-	};
-	
+		console.log("The " + whatvehicle.vehicle + " gets " + whatvehicle.MPG + " Miles Per Gallon and is suitable for " 
+		+ whatvehicle.weather + " weather.");
+	};	
 };
-// Array decloration
-var weather = [
-	"Sunny", 
-	"Cloudy", 
-	"Storming"
-	
+// Array decloration for What day of the week weather forcast
+var dayOfWeek = [
+	"Monday", 
+	"Tuesday", 
+	"Wednesday",
+	"Thursday",
+	"Friday",	
 ]; 
+// Array decloration- weather for the week
+var weatherForcast = [ 
+	"Sunny", 
+	"Sunny", 
+	"Storming",
+	"Sunny",
+	"Cloudy"
+];
+	
+console.log("Depending on the weather forcast for the week I will drive a specific vehicle in the hopes of staying under my " 
+				+ gasBudget + " dollar gas budget!");
+				
+console.log("The data for each vehicle is listed here:");
+
+jsonData(json);	
+
+console.log("I was watching the weather report on sunday is as follows");			
+				
+console.log("The five day forcast for the week is:");
+
+// For loop for predicting the weather forcast for the week
+for (var i=0, j=dayOfWeek.length; i < j; i++) {
+	console.log( "The forcast for " + dayOfWeek[i] + " is " + weatherForcast[i] );	
+};
+
 
 	
-
-var milesPerDay = 30;
-var gasBudget = 30;
-	
-	
-	
-
-jsonData(json);
