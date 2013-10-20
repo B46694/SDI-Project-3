3a476a843f18fea5d92792b94d0405e1d8535c65
Brannon DuBosee <brannondubose@me.com>
2013-10-19 19:14:42 -0500

    for loop, array, and more variables

--- SDI Project 3 Work/DuBose_B.js
+++ SDI Project 3 Work/DuBose_B.js
@@ -1,38 +1,47 @@
 //alert("JavaScript works!");
 
-// JSON Data, weather declarations
-	
+// JSON Data, weather declarations	
 var json = {
 	"whatvehicle": [
 		{
 				"weather": "Sunny", 
 				"vehicle": "CBR500r",
-				"MPG": 70
-			
+				"MPG": 70			
 		},
 		{
 				"weather": "Cloudy", 
 				"vehicle": "Mustang",
 				"MPG": 28
-			
 		},
 		{
 				"weather": "Storming", 
 				"vehicle": "Jeep",
-				"MPG": 11
-							
+				"MPG": 11							
 		}
-	]
-	
+	]	
 };
-
-var handleData = function (json) {
+// calling the jason data into a function
+var jsonData = function (json) {
 	for (var i = 0; i < json.whatvehicle.length; i++){
 		var whatvehicle = json.whatvehicle[i];
 		console.log("Weather: " + whatvehicle.weather + ", Vehicle: " + whatvehicle.vehicle + ", MPG: " + whatvehicle.MPG);
 	};
 	
 };
+// Array decloration
+var weather = [
+	"Sunny", 
+	"Cloudy", 
+	"Storming"
+	
+]; 
 
 	
-handleData(json);
+
+var milesPerDay = 30;
+var gasBudget = 30;
+	
+	
+	
+
+jsonData(json);
